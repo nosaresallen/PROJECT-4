ff7de2431c2a90938916f505a79ca6c974276ad5
nosaresallen <nosaresallen@outlook.com>
2023-12-10 01:48:24 +0800

    remove unnecessary codes

--- src/pages/AddEmployee.jsx
+++ src/pages/AddEmployee.jsx
@@ -128,193 +128,194 @@ export default function AddEmployee() {
                 Add Employee
             </Typography>
 
-            <Box component="form" noValidate  sx={{ mt: 3 }}>
+            <Box component="form" noValidate  sx={{ mt: 3 }}> 
                 <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                        autoComplete="given-name"
-                        name="firstname"
-                        required
-                        fullWidth
-                        id="firstname"
-                        label="First Name"
-                        variant='outlined'
-                        autoFocus
-                        size="medium"
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            firstname: e.target.value
-                        })}
-                        value={employee.firstname}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                        required
-                        fullWidth
-                        id="lastname"
-                        label="Last Name"
-                        name="lastname"
-                        variant='outlined'
-                        size="medium"
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            lastname: e.target.value
-                        })}
-                        value={employee.lastname}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                        required
-                        fullWidth
-                        id="address"
-                        label="Address"
-                        name="address"
-                        variant='outlined'
-                        size="medium"
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            address: e.target.value
-                        })}
-                        value={employee.address}
-                        />
-                    </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    autoComplete="given-name"
+                    name="firstname"
+                    required
+                    fullWidth
+                    id="firstname"
+                    label="First Name"
+                    variant='outlined'
+                    autoFocus
+                    size="medium"
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        firstname: e.target.value
+                    })}
+                    value={employee.firstname}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="lastname"
+                    label="Last Name"
+                    name="lastname"
+                    variant='outlined'
+                    size="medium"
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        lastname: e.target.value
+                    })}
+                    value={employee.lastname}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="address"
+                    label="Address"
+                    name="address"
+                    variant='outlined'
+                    size="medium"
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        address: e.target.value
+                    })}
+                    value={employee.address}
+                    />
+                </Grid>
 
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                        required
-                        fullWidth
-                        name="contact"
-                        label="Contact"
-                        type="number"
-                        id="contact"
-                        variant='outlined'
-                        size="medium"
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            contact: e.target.value
-                        })}
-                        value={employee.contact}
-                        inputProps={{
-                            min: 0,
-                        }}
-                        />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                    <FormControl fullWidth>
-                    <InputLabel id="gender">Gender*</InputLabel>
-                        <Select
-                        
-                        required
-                        fullWidth
-                        name="gender"
-                        type="gender"
-                        labelId="gender"
-                        id="gender"
-                        variant='outlined'
-                        size="medium"
-                        label="Gender"
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            gender: e.target.value
-                        })}
-                        
-                        value={employee.gender}
-                        
-                        >
-                            <MenuItem value={'Male'}>Male</MenuItem>
-                            <MenuItem value={'Female'}>Female</MenuItem>
-                            <MenuItem value={'Other'}>Other</MenuItem>
-                        </Select>
-                        </FormControl>
-                    </Grid>
-
-                    
-                    
-                    <Grid item xs={12}>
-                        <TextField
-                        required
-                        fullWidth 
-                        id="email"
-                        label="Email"
-                        name="email"
-                        variant='outlined'
-                        size="medium"
-                        onChange={handleEmailChange}
-                        value={employee.email}
-                        error={error}
-                        helperText={error ? 'Invalid email format' : ''}
-                        InputProps={{
-                        type: 'email',
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    required
+                    fullWidth
+                    name="contact"
+                    label="Contact"
+                    type="number"
+                    id="contact"
+                    variant='outlined'
+                    size="medium"
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        contact: e.target.value
+                    })}
+                    value={employee.contact}
+                    inputProps={{
+                        min: 0,
                     }}
-                        />
-                        
-                    </Grid>
+                    />
+                </Grid>
 
-                    <Grid item xs={12}>
-                    <FormControl fullWidth >
-                    <InputLabel id="position">Position*</InputLabel>
-                        <Select
-                        
-                        required
-                        fullWidth
-                        name="position"
-                        
-                        type="position"
-                        labelId="position"
-                        id="position"
-                        variant='outlined'
-                        size="medium"
-                        label="Position"
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            position: e.target.value
-                        })}
-                        
-                        value={employee.position}
-                        
-                        >
-                            <MenuItem value={'Project Manager'}>Project Manager</MenuItem>
-                            <MenuItem value={'Software Engineer'}>Software Engineer</MenuItem>
-                            <MenuItem value={'UI/UX Designer'}>UI/UX Designer</MenuItem>
-                            <MenuItem value={'Front End Developer'}>Front End Developer</MenuItem>
-                            <MenuItem value={'Back End Developer'}>Back End Developer</MenuItem>
-                        </Select>
-                        </FormControl>
-                    </Grid>
+                <Grid item xs={12} sm={6}>
+                <FormControl fullWidth>
+                <InputLabel id="gender">Gender*</InputLabel>
+                    <Select
+                    
+                    required
+                    fullWidth
+                    name="gender"
+                    type="gender"
+                    labelId="gender"
+                    id="gender"
+                    variant='outlined'
+                    size="medium"
+                    label="Gender"
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        gender: e.target.value
+                    })}
+                    
+                    value={employee.gender}
+                    
+                    >
+                        <MenuItem value={'Male'}>Male</MenuItem>
+                        <MenuItem value={'Female'}>Female</MenuItem>
+                        <MenuItem value={'Other'}>Other</MenuItem>
+                    </Select>
+                    </FormControl>
+                </Grid>
 
-                    <Grid item xs={12}>
-                        <TextField
-                        required
-                        fullWidth
-                        name="hiredate"
-                        label="Hire Date " 
-                        type="date"
-                        id="hiredate"
-                        variant='outlined'
-                        size="medium"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        onChange={(e)=>setEmployee({
-                            ...employee,
-                            hiredate: e.target.value
-                        })}
-                        value={employee.hiredate}
-                        />
-                        
-                    </Grid>
+                
+                
+                <Grid item xs={12}>
+                    <TextField
+                    required
+                    fullWidth 
+                    id="email"
+                    label="Email"
+                    name="email"
+                    variant='outlined'
+                    size="medium"
+                    onChange={handleEmailChange}
+
+                    value={employee.email}
+                    error={error}
+                    helperText={error ? 'Invalid email format' : ''}
+                    InputProps={{
+                    type: 'email',
+                }}
+                    />
+                    
+                </Grid>
+
+                <Grid item xs={12}>
+                <FormControl fullWidth >
+                <InputLabel id="position">Position*</InputLabel>
+                    <Select
+                    
+                    required
+                    fullWidth
+                    name="position"
+                    
+                    type="position"
+                    labelId="position"
+                    id="position"
+                    variant='outlined'
+                    size="medium"
+                    label="Position"
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        position: e.target.value
+                    })}
+                    
+                    value={employee.position}
+                    
+                    >
+                        <MenuItem value={'Project Manager'}>Project Manager</MenuItem>
+                        <MenuItem value={'Software Engineer'}>Software Engineer</MenuItem>
+                        <MenuItem value={'UI/UX Designer'}>UI/UX Designer</MenuItem>
+                        <MenuItem value={'Front End Developer'}>Front End Developer</MenuItem>
+                        <MenuItem value={'Back End Developer'}>Back End Developer</MenuItem>
+                    </Select>
+                    </FormControl>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <TextField
+                    required
+                    fullWidth
+                    name="hiredate"
+                    label="Hire Date " 
+                    type="date"
+                    id="hiredate"
+                    variant='outlined'
+                    size="medium"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    onChange={(e)=>setEmployee({
+                        ...employee,
+                        hiredate: e.target.value
+                    })}
+                    value={employee.hiredate}
+                    />
+                    
+                </Grid>
                 
                 </Grid>
                 <Button
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2, bgcolor: grey[900],transition: 'background-color 0.3s',
-                '&:hover': {
-                    backgroundColor: 'teal',
-                }, }}
+                sx={{ mt: 3, mb: 2, bgcolor: grey[900],
+                    transition: 'background-color 0.3s',
+                    '&:hover': {
+                        backgroundColor: 'teal'}, }}
                 
                 onClick={()=>addEmployee()}
                 >
